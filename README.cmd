351835c6757fd4b33f337663cd0ff4d38ca97792
gbengataylor <gb@redhat.com>
2020-04-07 11:21:45 -0400

    update

--- README.cmd
+++ README.cmd
@@ -1,3 +1,5 @@
+Deploying https://hub.docker.com/_/intersystems-iris-data-platform/plans/222f869e-567c-4928-b572-eb6a29706fbd?tab=instructions
+
 kompose convert -f iris-compose.yaml
 
 #add service account to allow privilege container
@@ -5,3 +7,5 @@ kompose convert -f iris-compose.yaml
 #if have dynamic provisioning, skip pvc
 
 # update the deployment to include service account and prehook checks
+
+# for now need to run oc exec [pod] -- iris session IRIS
